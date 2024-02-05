572ad4ced44b63f9c8093c5d388a10c75964f342
Rojotiana Nomena <hei.nomena@gmail.com>
2024-02-05 18:20:46 +0300

    test: carManual builder

--- src/test/java/BuilderTest.java
+++ src/test/java/BuilderTest.java
@@ -1,8 +1,11 @@
 import com.implementation.builder.CarBuilder;
+import com.implementation.builder.CarManualBuilder;
 import com.implementation.builder.Director;
 import com.implementation.model.Car;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.implementation.model.CarManual;
 import org.junit.jupiter.api.Test;
 
 public class BuilderTest {
@@ -21,5 +24,22 @@ public class BuilderTest {
         assertEquals("Sport Engine", car.getEngine().getType());
         assertEquals("Gps", car.getGps());
         assertEquals("TripComputer", car.getTripComputer());
+    }
+
+    @Test
+    public void buildManual(){
+        Director director = new Director();
+        CarManualBuilder builder = new CarManualBuilder();
+        builder.setSeats(2);
+        builder.setGPS();
+        builder.setTripComputer();
+        director.makeSuv(builder);
+
+        CarManual manual = builder.getResult();
+
+        assertEquals(2, manual.getSeats());
+        assertEquals("SUV Engine", manual.getEngine().getType());
+        assertEquals("Gps", manual.getGps());
+        assertEquals("TripComputer", manual.getTripComputer());
     }
 }
